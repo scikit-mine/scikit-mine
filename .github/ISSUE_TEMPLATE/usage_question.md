97b160aa3c29041d8ff68bdbf54b3a5791b047b5
Rémi Adon <remi.adon@gmail.com>
2020-04-29 17:53:50 +0200

    RM everything but docs

--- .github/ISSUE_TEMPLATE/usage_question.md
+++
@@ -1,7 +0,0 @@
----
-name: Usage question
-about: If you have a usage question
-title: ''
-labels: question
-assignees: ''
----
--- skmine/datasets/tests/test_base.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-
-from skmine.datasets import get_data_home
-
-
-def test_data_home():
-    assert os.path.exists(get_data_home())
