9c0b7fee168edfccf5ef0e3a7bb62c19675391df
radon <remi.adon@gmail.com>
2020-04-30 12:29:46 +0200

    [ADD] make_transactions function

--- skmine/datasets/__init__.py
+++ skmine/datasets/__init__.py
@@ -3,6 +3,7 @@ The :mod:`skmine.datasets` module includes utilities to load datasets,
 including methods to load and fetch popular reference datasets.
 """
 from functools import partial
+from ._samples_generator import make_transactions
 
 from ._base import get_data_home
 _REQ_TIMEOUT = 8
