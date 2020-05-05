db7262a9d6cad1ef89638271343b8e3cf0b58390
radon <remi.adon@gmail.com>
2020-05-05 15:36:45 +0200

    make_transactions take np.random_state as arg

--- skmine/datasets/tests/test_samples_generator.py
+++ skmine/datasets/tests/test_samples_generator.py
@@ -22,13 +22,28 @@ def test_bilateral():
     check consistency by generating a transactional dataset with given properties
     and retrieving these properties in the post-hoc description of this dataset
     """
-    properties = dict(
-        n_transactions=150,
-        n_items=30,
-        avg_transaction_size=20,
-    )
+    properties = dict(n_transactions=150, n_items=30, avg_transaction_size=15)
     D = make_transactions(**properties)
     desc = describe_itemsets(D)
-    assert desc['n_transactions'] == properties['n_transactions']
-    assert desc['n_items'] == properties['n_items']
-    assert round(desc['avg_transaction_size']) == properties['avg_transaction_size']
+    assert desc == pytest.approx(properties, abs=1.0)
+
+
+def test_bilateral_n_items_too_low():
+    """
+    If n_items if too low to fill transactions w.r.t avg_transaction_size,
+    we automatically adapt
+    """
+    properties = dict(n_transactions=150, n_items=30, avg_transaction_size=20)
+    with pytest.raises(ValueError):
+        D = make_transactions(**properties)
+
+
+
+def test_random_state():
+    """hard setting of ``random_state`` should make two generated datasets really close"""
+    properties = dict(n_transactions=150, n_items=30, avg_transaction_size=15, random_state=2)
+    D1 = make_transactions(**properties)
+    D2 = make_transactions(**properties)
+    desc1 = describe_itemsets(D1)
+    desc2 = describe_itemsets(D2)
+    assert desc1 == pytest.approx(desc2, abs=.1)
