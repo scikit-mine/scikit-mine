757770753c73dc8344a249c8308f0a8159ffe3cd
radon <remi.adon@gmail.com>
2020-05-05 15:46:12 +0200

    benchmark lcm with new random generation

--- asv_bench/benchmarks/lcm.py
+++ asv_bench/benchmarks/lcm.py
@@ -1,24 +1,5 @@
 from skmine.itemsets import LCM
-import numpy as np
-import pandas as pd
-import warnings
-
-def make_transactions(n_transactions, n_items, avg_transaction_size, seed=None):
-    if avg_transaction_size > n_items:
-        warnings.warn(
-            'Average transaction size bigger than n_items'
-        )
-
-    choices = np.arange(n_items)
-    np.random.seed(seed)
-    t_sizes = np.random.binomial(
-        n=min(avg_transaction_size * 2, n_items),  # avoid more than n_items in single trans
-        p=.5,  # centered around avg_transaction_size
-        size=n_transactions
-    )
-
-    D = [np.random.choice(choices, size, replace=False) for size in t_sizes]
-    return pd.Series(D)
+from skmine.datasets import make_transactions
 
 class TimeLCM:
     params = ([.1, .3, .5], [20], [50, 100])
@@ -32,11 +13,11 @@ class TimeLCM:
             n_transactions,
             n_items,
             avg_transaction_size,
-            seed=7
+            random_state=7
         )
         self.lcm = LCM(min_supp=min_supp, n_jobs=1)  # set n_jobs to 1 for now
 
-    def time_fit(self, *args):
+    def time_fit_only(self, *args):
         self.lcm.fit(self.transactions)
 
     def time_fit_transform(self, *args):
