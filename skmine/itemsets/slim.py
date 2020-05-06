d91f9cd1428bc3856a9b069dd87b45820aebb8cb
radon <remi.adon@gmail.com>
2020-05-06 15:14:16 +0200

    SLIM first implem no pruning

--- skmine/itemsets/slim.py
+++ skmine/itemsets/slim.py
@@ -5,6 +5,7 @@
 
 from collections import defaultdict
 from functools import reduce
+from itertools import chain
 
 import numpy as np
 import pandas as pd
@@ -30,14 +31,15 @@ def cover_one(codetable, cand):
     """
     cover = list()
     stack = set()
-    for iset in codetable:
+    pos = 0
+    while len(stack) < len(cand) and pos < len(codetable):
+        iset = codetable[pos]
+        pos += 1
         if not iset.isdisjoint(stack):
             continue
         if iset.issubset(cand):
             cover.append(iset)  # TODO add index instead of element for performance
             stack |= iset
-        if len(stack) >= len(cand):
-            break
     return cover
 
 
@@ -67,7 +69,7 @@ class SLIM(BaseMiner): # TODO : inherit MDLOptimizer
         self.supports = lazydict(self.get_support)
         self.model_size = None          # L(CT|D)
         self.data_size = None           # L(D|CT)
-    
+
     def get_support(self, itemset):
         U = reduce(RoaringBitmap.union, self.standard_codetable.loc[itemset])
         return len(U)
@@ -84,15 +86,75 @@ class SLIM(BaseMiner): # TODO : inherit MDLOptimizer
     def _prefit(self, D: pd.Series):
         self.standard_codetable = make_codetable(D)
         usage = self.standard_codetable.map(len).astype(np.uint32)
-        self.codetable = self.standard_codetable.copy()
-        self.codetable.index = self.codetable.index.map(lambda e: frozenset([e]))  # singletons
-        self.codetable = self.codetable.reindex(usage.sort_values(ascending=False).index)
 
-        codes = -np.log2(usage / usage.sum()) 
+        sorted_index = sorted(usage.index, key=lambda e: (-usage[e], e))
+        self.codetable = self.standard_codetable.reindex(sorted_index, copy=True)
+        self.codetable.index = self.codetable.index.map(lambda e: frozenset([e]))
+
+        codes = -np.log2(usage / usage.sum())
         self.model_size = 2 * codes.sum()      # L(code_ST(X)) = L(code_CT(X)), because CT=ST
         self.data_size = (codes * usage).sum()
 
         return self
 
 
-    def fit(self): pass  # TODO
+    def get_standard_codes(self, index):
+        """compute the size of a codetable index given the standard codetable"""
+        flat_items = list(chain(*index))
+        items, counts = np.unique(flat_items, return_counts=True)
+
+        usages = self.standard_codetable.loc[items].map(len).astype(np.uint32)
+        usages /= usages.sum()
+        codes = -np.log2(usages)
+        return codes * counts
+
+    def compute_sizes(self, codetable):
+        """
+        Parameters
+        ----------
+        codetable : pd.Series
+            A series mapping itemsets for their tids
+            TODO : no need for tids here, only usages
+
+        Returns
+        -------
+        tuple(int, int)
+            (data_size, model_size)
+        """
+        usages = codetable.map(len).astype(np.uint32)
+        codes = -np.log2(usages / usages.sum())
+
+        stand_codes = self.get_standard_codes(codetable.index)
+
+        model_size = stand_codes.sum() + codes.sum() # L(CTc|D) = L(X|ST) + L(X|CTc)
+        data_size = (codes * usages).sum()
+        return data_size, model_size
+
+    def fit(self, D):
+        self._prefit(D)
+        CTc_index = None
+        n_iter_no_change = 0
+        is_better = True
+        while n_iter_no_change < self.n_iter_no_change:
+            candidates = generate_candidates(self.codetable)
+            for cand in candidates:
+                CT_index = self.codetable.index
+                cand_pos = self.get_cover_order_pos(CT_index, cand)
+                CTc_index = CT_index.insert(cand_pos, cand)
+
+                covers = D.map(lambda t: cover_one(CTc_index, t))
+                CTc = make_codetable(covers)
+                data_size, model_size = self.compute_sizes(CTc)
+
+                if data_size + model_size < self.data_size + model_size:
+                    zeros_index = self.codetable.index.difference(CTc.index)
+                    CTc = CTc.reindex(CTc_index)
+                    for itemset in zeros_index:
+                        CTc[itemset] = RoaringBitmap()
+                    self.codetable = CTc
+                    self.data_size = data_size
+                    self.model_size = model_size
+                else:
+                    is_better = False
+            if not is_better:
+                n_iter_no_change += 1
--- skmine/itemsets/tests/test_slim.py
+++ skmine/itemsets/tests/test_slim.py
@@ -77,8 +77,74 @@ def test_cover_order_pos_2():
 
 
 def test_prefit():
-    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    D = ['ABC'] * 5 + ['BC', 'B', 'C']
     slim = SLIM()
     slim._prefit(D)
     np.testing.assert_almost_equal(slim.model_size, 9.614, 3)
     np.testing.assert_almost_equal(slim.data_size, 29.798, 3)
+    assert len(slim.codetable) == 3
+    assert slim.codetable.dtype == np.object
+    assert slim.codetable.index.tolist() == list(map(frozenset, ['B', 'C', 'A']))
+
+def test_get_standard_size_1():
+    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    slim = SLIM()
+    slim._prefit(D)
+    CT_index = ['ABC', 'AB', 'A', 'B']
+    codes = slim.get_standard_codes(CT_index)
+    pd.testing.assert_series_equal(
+        codes,
+        pd.Series([4.32, 4.32, 1.93], index=list('ABC')),
+        check_less_precise=2
+    )
+
+def test_get_standard_size_2():
+    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    slim = SLIM()
+    slim._prefit(D)
+    CT_index = ['ABC', 'A', 'B']
+    codes = slim.get_standard_codes(CT_index)
+    pd.testing.assert_series_equal(
+        codes,
+        pd.Series([2.88, 2.88, 1.93], index=list('ABC')),
+        check_less_precise=2
+    )
+
+
+def test_compute_sizes_1():
+    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    slim = SLIM()
+    slim._prefit(D)
+    CT = pd.Series({
+        frozenset('ABC'): RoaringBitmap(range(0, 5)),
+        frozenset('AB'): RoaringBitmap([5]),
+        frozenset('A'): RoaringBitmap([6]),
+        frozenset('B'): RoaringBitmap([7]), 
+    })
+
+    data_size, model_size = slim.compute_sizes(CT)
+    np.testing.assert_almost_equal(data_size, 12.4, 2)
+    np.testing.assert_almost_equal(model_size, 20.25, 2)
+
+
+def test_compute_sizes_2():
+    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    slim = SLIM()
+    slim._prefit(D)
+    CT = pd.Series({
+        frozenset('ABC'): RoaringBitmap(range(0, 5)),
+        frozenset('A'): RoaringBitmap([5, 6]),
+        frozenset('B'): RoaringBitmap([5, 7]),
+    })
+
+    data_size, model_size = slim.compute_sizes(CT)
+    np.testing.assert_almost_equal(data_size, 12.92, 2)
+    np.testing.assert_almost_equal(model_size, 12.876, 2)
+
+
+def test_fit():
+    D = ['ABC'] * 5 + ['AB', 'A', 'B']
+    D = pd.Series(D)
+    slim = SLIM()
+    slim.fit(D)
+    assert slim.codetable.index.tolist() == list(map(frozenset, ['ABC', 'AB', 'A', 'B', 'C']))
