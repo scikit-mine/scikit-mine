f5f781f33bf9ff35a1b876d6f46e4ba05b42df43
radon <remi.adon@gmail.com>
2020-05-28 10:30:00 +0200

    SLIM bench add preprocessing

--- asv_bench/benchmarks/slim.py
+++ asv_bench/benchmarks/slim.py
@@ -1,22 +1,25 @@
 from skmine.itemsets import SLIM
 from skmine.datasets import make_transactions
+from skmine.preprocessing import TransactionEncoder
 
 class SLIMBench:
-    params = ([20], [.4, .6], [False, True])
+    params = ([20], [.2, .4], [False, True])
     param_names = ['n_transactions', 'density', 'pruning']
     #timeout = 20  # timeout for a single run, in seconds
     repeat = (1, 3, 20.0)
     processes = 1
     def setup(self, n_transactions, density, pruning):
-        self.transactions = make_transactions(
+        transactions = make_transactions(
             n_transactions=n_transactions,
             density=density,
             random_state=7,
         )
+        self.transactions = TransactionEncoder().fit_transform(transactions)
+
         self.slim = SLIM(pruning=pruning)
 
     def time_fit(self, *args):
-        self.slim.fit(self.transactions)
+        return self.slim.fit(self.transactions)
 
     def mem_fit(self, *args):
         return self.slim.fit(self.transactions)
