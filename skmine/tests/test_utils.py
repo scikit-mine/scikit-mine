279762f97a73a652749922d624688b4021400a4e
radon <remi.adon@gmail.com>
2020-07-21 16:01:40 +0200

    [RM] useless lazydict

--- skmine/tests/test_utils.py
+++ skmine/tests/test_utils.py
@@ -1,19 +1,8 @@
 import pytest
 import numpy as np
-from ..utils import lazydict
 from ..utils import _check_random_state
 from ..utils import _check_min_supp
 from ..utils import _check_growth_rate
-
-def test_lazydict():
-    d = lazydict(lambda e: e*2)
-    assert d[2] == 4
-    assert 2 in d.keys()
-
-def test_lazydict_no_default():
-    d = lazydict()
-    with pytest.raises(KeyError):
-        d[3]
 
 def test_check_random_state():
     random_state = np.random.RandomState(18)
--- skmine/utils.py
+++ skmine/utils.py
@@ -2,29 +2,11 @@
 utils functions
 """
 
-from collections import defaultdict
 from functools import partial
 from operator import gt, lt
 
 import numpy as np
 import pandas as pd
-
-
-class lazydict(defaultdict):
-    """
-    lazydict(default_factory[, ...]) --> dict with default factory
-
-    The default factory is called with key as argument to produce
-    a new value (via  __getitem__ only), and store it.
-    A lazydict compares equal to a dict with the same items.
-    All remaining arguments are treated the same as if they were
-    passed to the dict constructor, including keyword arguments.
-    """
-    def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError(key)
-        res = self[key] = self.default_factory(key)
-        return res
 
 
 def _check_random_state(random_state):
