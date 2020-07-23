f541c4153e3b096506d7efc955dca591739809ff
radon <remi.adon@gmail.com>
2020-07-23 11:14:51 +0200

    try fixing callback error from CI

--- skmine/callbacks.py
+++ skmine/callbacks.py
@@ -31,7 +31,7 @@ def post(self, func_name, callback):
             callback(self)
         elif callback_params:
             try:
-                callback(res_)
+                callback(*res_)
             except TypeError:
                 callback(*res)
         else:
