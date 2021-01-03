4a0561a36ee8a1596c84c47a4f7d82c902ca79eb
remiadon <remi.adon@protonmail.com>
2021-01-03 15:36:50 +0000

    Deploying to gh-pages from  @ 1362318dc0b18308b455a4d11291b1fe7544c040 🚀

--- build/lib/skmine/emerging/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The :mod:`skming.emerging` module includes methods and classes
-dedicated to the problem of finding emerging patterns
-"""
-from .mbdldorber import MBDLLBorder
--- build/lib/skmine/preprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The :mod:`skmine.preprocessing` module includes utilities and transformers
-to make the data ingestion easier
-"""
-
-from .lcm import LCM
-from .transaction_encoder import TransactionEncoder
-from .mdlp_discretizer import MDLPDiscretizer
