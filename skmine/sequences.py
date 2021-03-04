6d4508020c898db4b5a289b6a9512a7b4c3d7a72
Rémi Adon <remi.adon@gmail.com>
2021-03-04 22:09:20 +0100

    GoKrimp : pyroaring-->roaringbitmap && [ADD] max_gap parameter

--- skmine/sequences.py
+++ skmine/sequences.py
@@ -1,9 +1,11 @@
+import warnings
 from collections import defaultdict
 import pandas as pd
 import numpy as np
 
-from pyroaring import BitMap as Bitmap
+from .bitmaps import Bitmap
 from .base import BaseMiner, MDLOptimizer
+
 
 def to_vertical(D):
     """
@@ -21,8 +23,8 @@ def to_vertical(D):
     return dict(event_to_indices), np.cumsum(seq_lengths)
 
 
-def compress_size(D, patt, seq_lengths):
-    """ Algorithm 2 from original paper
+def compress_size(D, patt, seq_lengths, *, max_gap=1):
+    """Algorithm 2 from original paper
 
     This version uses vertical representations of the datasets
     "Pointers" mentioned in the paper are bitmaps pointing to start positions
@@ -38,29 +40,34 @@ def compress_size(D, patt, seq_lengths):
     seq_lengths: np.array of shape(n_transactions, )
         a 1-D array containing transaction lengths
 
+    max_gap: int
+        maximum number of gaps in a pattern
+
     TODO add max_gap=2 and interleaving=True
     """
     cost = len(patt) + sum(map(len, D.values()))
     # sum(D.values()) vertical eq. to sum(len(Si) for Si in horizontal D)
-    D = {k: D[k].copy() for k in patt}  # check in pattern, other events are left untouched anyway
+    D = {
+        k: D[k].copy() for k in patt
+    }  # check in pattern, other events are left untouched anyway
     patt = tuple(patt)
 
     start_positions = D[patt[0]].copy()  # iter over positions for first element of p
-    start_seq_indices = np.searchsorted(seq_lengths, start_positions, side='right')
+    start_seq_indices = np.searchsorted(seq_lengths, start_positions, side="right")
     seq_end_indices = seq_lengths[start_seq_indices]
     assert (seq_end_indices >= start_seq_indices).all()
     for start_pos, seq_end_idx in zip(start_positions, seq_end_indices):
         pos = start_pos
         indices = [pos]
+        gap_ctr = max_gap
         for e in patt[1:]:
-            bits = D[e]
-            if pos > seq_end_idx:
-                break
-            if bits and pos <= bits.max():  # avoid error
-                next_set_bit = bits.next_set_bit(pos)
+            bits = D[e].clamp(pos + 1, min(pos + gap_ctr + 1, seq_end_idx))
+            if bits:
+                next_set_bit = bits.min()
+                gap_ctr -= next_set_bit - pos - 1  # consume gap
                 indices.append(next_set_bit)
                 pos = next_set_bit
-        if len(indices) == len(patt):
+        if len(indices) == len(patt):  # found entire pattern
             bm = Bitmap(indices)  # TODO use regular lists
             for e in patt:
                 D[e] -= bm
@@ -76,7 +83,7 @@ def compress_size(D, patt, seq_lengths):
     return cost, D
 
 
-def best_compressing(D, seq_lengths):
+def best_compressing(D, seq_lengths, *, max_gap=1):
     """
     get the best compressing pattern from the dataset D
     """
@@ -86,9 +93,11 @@ def best_compressing(D, seq_lengths):
 
     while len(D) - len(p) > 0:
         cands = sorted(D.keys() - p)  # lexicographical order
-        e = max(cands, key=lambda k: (len(D[k]), estimate_gain(D[k], seq_lengths)))  # TODO : lazy estimate gain
+        e = max(
+            cands, key=lambda k: (len(D[k]), estimate_gain(D[k], seq_lengths))
+        )  # TODO : lazy estimate gain
         p.append(e)
-        comp_size, u_d = compress_size(D, p, seq_lengths)
+        comp_size, u_d = compress_size(D, p, seq_lengths, max_gap=max_gap)
         if comp_size < b:
             b = comp_size
             update_d = u_d
@@ -96,7 +105,8 @@ def best_compressing(D, seq_lengths):
             if len(p) > 1:
                 p.pop(-1)
             break
-    return tuple(p), update_d
+    return tuple(p), update_d  # TODO : return b ?
+
 
 def estimate_gain(tids, seq_lengths):
     """
@@ -106,15 +116,13 @@ def estimate_gain(tids, seq_lengths):
     We take sequences lengths into account, so we dont mistakenly favor
     events from different transactions
     """
-    from itertools import takewhile
     diffs = []
     for i in range(len(seq_lengths) - 1):
-        start, end = seq_lengths[i: i + 2]
-        _tids = list(takewhile(lambda e: e < end, tids.iter_equal_or_larger(start)))
+        start, end = seq_lengths[i : i + 2]
+        _tids = tids.clamp(start, end)
         diff = np.diff(_tids)
         diffs.append(diff.mean())
     return np.mean(diffs)
-
 
 
 class GoKrimp(BaseMiner, MDLOptimizer):  # TODO : inerit MDL Optimizer
@@ -126,7 +134,9 @@ class GoKrimp(BaseMiner, MDLOptimizer):  # TODO : inerit MDL Optimizer
 
     def fit(self, D):
         vert_D, cum_seq_lengths = to_vertical(D)
-        self.standard_codetable_ = {event : tids.copy() for event, tids in vert_D.items()}
+        self.standard_codetable_ = {
+            event: tids.copy() for event, tids in vert_D.items()
+        }
 
         H = dict()
         while len(H) < self.k_:
@@ -144,9 +154,8 @@ class GoKrimp(BaseMiner, MDLOptimizer):  # TODO : inerit MDL Optimizer
         return self
 
     def discover(self):
-        cpy = {pattern : tids.copy() for pattern, tids in self.codetable_.items()}
+        cpy = {pattern: tids.copy() for pattern, tids in self.codetable_.items()}
         return pd.Series(cpy.values(), index=list(cpy.keys()))
-
 
     def generate_candidates(self):
         """
@@ -160,16 +169,24 @@ class GoKrimp(BaseMiner, MDLOptimizer):  # TODO : inerit MDL Optimizer
         """
         yield best_compressing(self.codetable_, self.seq_lengths)
 
-
     def evaluate(self, patt):
-        pass  # TODO 
+        pass  # TODO
 
+    def cover(self, D):
+        pass  # TODO
 
     def reconstruct(self):
         """
         1. reconstruct one big sequence
         2. split it into the original sequences giving the cumulative sums of the transaction lengths
         """
+        warnings.warn(
+            """
+            GoKRIMP has a lossy compression scheme,
+            hence reconconstrution is performed from the
+            standard codetable
+        """
+        )
         # last of cumulative sequence lengths if the total size of the flattened sequence
         flat = np.empty(self._cum_seq_lengths[-1], dtype=object)
         flat[:] = None
@@ -178,6 +195,6 @@ class GoKrimp(BaseMiner, MDLOptimizer):  # TODO : inerit MDL Optimizer
 
         l = list()
         for i in range(len(self._cum_seq_lengths) - 1):
-            start, end = self._cum_seq_lengths[i: i + 2]
-            l.append(flat[start: end].tolist())
+            start, end = self._cum_seq_lengths[i : i + 2]
+            l.append(flat[start:end].tolist())
         return pd.Series(l)
