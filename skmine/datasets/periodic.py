4c1fb8226dd8ff068ba5e6731cdeddbafad87ee4
Rémi Adon <remi.adon@gmail.com>
2021-04-07 11:49:16 +0200

    [ADD] fetch_canadian_tv method

--- skmine/datasets/periodic.py
+++ skmine/datasets/periodic.py
@@ -2,27 +2,30 @@
 Base IO for all periodic datasets
 """
 import os
+from zipfile import ZipFile
 
 import pandas as pd
 
 from ._base import get_data_home
+from .conf import urlopen
 
 
 def fetch_health_app(data_home=None, filename="health_app.csv"):
     """Fetch and return the health app log dataset
-    from github.com/logpai/loghub
+
+    see: https://github.com/logpai/loghub
 
     HealthApp is a mobile application for Android devices.
     Logs were collected from an Android smartphone after 10+ days of use.
 
-    This dataset only represents the different types of logs, hence resulting
+    Logs have been grouped by their types, hence resulting
     in only 20 different events.
 
-    ==============================      ==================================
+    ==============================      ===================================
     Number of events                    20
     Average delta per event             Timedelta('0 days 00:53:24.984000')
     Average nb of points per event      100.0
-    ========================            ===================================
+    ==============================      ===================================
 
     Parameters
     ----------
@@ -33,8 +36,8 @@ def fetch_health_app(data_home=None, filename="health_app.csv"):
     Returns
     -------
     pd.Series
-        Transactions from the health app dataset, as an in-memory pandas Series.
-        Each unique transaction is represented as a Python list.
+        System logs from the health app dataset, as an in-memory pandas Series.
+        Events are indexed by timestamps.
     """
     data_home = data_home or get_data_home()
     p = os.path.join(data_home, filename)
@@ -53,4 +56,82 @@ def fetch_health_app(data_home=None, filename="health_app.csv"):
     s.index.name = "timestamp"
     s.index = pd.to_datetime(s.index, format="%Y%m%d-%H:%M:%S:%f")
 
+    return s
+
+
+def _extract_canadian_tv(zip_name, filename, data_home):
+    resp = urlopen(
+        "https://applications.crtc.gc.ca/OpenData/Television%20Logs/STAR2/2020/2020-08.zip"
+    )
+    with open(zip_name, "wb") as fd:
+        fd.write(resp.read())
+    zf = ZipFile(zip_name)
+    zf.extract(filename, path=data_home)
+
+
+def fetch_canadian_tv(data_home=None, filename="2020-08\CBC_202008_140114251.log"):
+    """
+    Fetch and return canadian TV logs from August 8, 2020
+
+    see: https://open.canada.ca/data/en/dataset/800106c1-0b08-401e-8be2-ac45d62e662e
+
+    If the dataset has never been downloaded before, it will be downloaded and stored.
+
+    The returned dataset contains only TV series episodes index by their associated timestamps
+    Adverts are ignored when loading the dataset.
+
+    ==============================      =======================================
+    Number of events                    131
+    Average delta per event             Timedelta('15 days 14:59:42.137404580')
+    Average nb of points per event      15.977099236641221
+    ==============================      =======================================
+
+    Parameters
+    ----------
+    data_home : optional, default: None
+        Specify another download and cache folder for the datasets. By default
+        all scikit-mine data is stored in `scikit-mine_data`.
+
+    Returns
+    -------
+    pd.Series
+        TV series events from canadian TV, as an in-memory pandas Series.
+        Events are indexed by timestamps.
+
+    Notes
+    -----
+    For now the entire .zip file is downloaded, being ~90mb on disk
+    Downloading preprocessd dataset from zenodo.org is something we consider.
+
+    See Also
+    -------
+    skmine.datasets.get_data_home
+
+    Examples
+    --------
+    >>> from skmine.datasets import fetch_canadian_tv
+    >>> ctv = fetch_canadian_tv()  # first time will take long
+    >>> ctv.head()
+    2020-08-01 06:00:00            The Moblees
+    2020-08-01 06:11:00    Big Block Sing Song
+    2020-08-01 06:13:00    Big Block Sing Song
+    2020-08-01 06:15:00               CBC Kids
+    2020-08-01 06:15:00               CBC Kids
+    dtype: object
+    """
+    data_home = data_home or get_data_home()
+    p = os.path.join(data_home, filename)
+    kwargs = dict(header=None, squeeze=True, dtype="string", sep="\t")
+    zip_name = filename.replace(".csv", ".zip")
+
+    if not filename in os.listdir(data_home):
+        _extract_canadian_tv(zip_name, filename, data_home)
+
+    s = pd.read_csv(p, **kwargs)
+    s = s.str.split("   ").str[1]  # TODO this is dirty. Store file on zenodo
+    s = s[s.str.match("\d+[^\d]+")]
+    index = s.str[:10]
+    data = s.str[24:]
+    s = pd.Series(data.values, index=index.values)
+    s.index = pd.to_datetime(s.index, format="%y%m%d%H%M")
     return s
--- skmine/datasets/tests/test_periodic.py
+++ skmine/datasets/tests/test_periodic.py
@@ -1,26 +1,55 @@
-from ..periodic import fetch_health_app
 import os
+
 import pandas as pd
+import pytest
+
+from .. import periodic
+from ..periodic import fetch_canadian_tv, fetch_health_app
 
 
 def mock_read_csv(*args, **kwargs):
     return pd.Series([2, 3], index=["20171223-22:15:29:606", "20171223-22:15:29:615"])
 
 
-def test_fetch_any(monkeypatch):
+def mock_read_csv_canadian_tv(*args, **kwargs):
+    return pd.Series(
+        [
+            "2PGR10CBC   200801060000061130001130The Moblees                                       1-16 Great Galloping Moblees                                  C3772721 35105ACD",
+            "2PGR10CBC   200801061130061330000200Big Block Sing Song                               2-009 CHICKENS                                                B3684421 351120CD",
+            "2PGR10CBC   200801061330061530000200Big Block Sing Song                               2-010 SPECTACULAR                                             B3684421 351120CD",
+            "2PGR10CBC   200801061530061535000005CBC Kids                                          19/20-3456, Coming Up - NapkinMan                 CBC               21 341120CC",
+        ]
+    )
+
+
+@pytest.mark.parametrize("already_downloaded", [True, False])
+def test_fetch_healt_app(monkeypatch, already_downloaded):
     name = "filename"
     monkeypatch.setattr(pd.Series, "to_csv", lambda *args, **kwargs: None)
 
     # force file to be in DATA_HOME, so we don't need to fetch it
-    monkeypatch.setattr(os, "listdir", lambda *args: ["{}.csv".format(name)])
-    monkeypatch.setattr(pd, "read_csv", mock_read_csv)
+    if already_downloaded:
+        monkeypatch.setattr(os, "listdir", lambda *args: ["{}.csv".format(name)])
+        monkeypatch.setattr(pd, "read_csv", mock_read_csv)
+    else:  # now DATA_HOME to be empty, file has to be fetched
+        monkeypatch.setattr(os, "listdir", lambda *args: list())
+        monkeypatch.setattr(pd, "read_csv", mock_read_csv)
+
     data = fetch_health_app(filename=name)
     assert data.shape == (2,)
     assert isinstance(data.index, pd.DatetimeIndex)
 
-    # now DATA_HOME to be empty, file has to be fetched
-    monkeypatch.setattr(os, "listdir", lambda *args: list())
-    monkeypatch.setattr(pd, "read_csv", mock_read_csv)
-    data = fetch_health_app()
-    assert data.shape == (2,)
+
+@pytest.mark.parametrize("already_downloaded", [True, False])
+def test_fetch_canadian_tv(monkeypatch, already_downloaded):
+    if already_downloaded:
+        monkeypatch.setattr(
+            os, "listdir", lambda *args: ["2020-08\CBC_202008_140114251.log"]
+        )
+    else:
+        monkeypatch.setattr(os, "listdir", lambda *args: [])
+        monkeypatch.setattr(periodic, "_extract_canadian_tv", lambda *args: None)
+    monkeypatch.setattr(pd, "read_csv", mock_read_csv_canadian_tv)
+    data = fetch_canadian_tv()
+    assert data.shape == (4,)
     assert isinstance(data.index, pd.DatetimeIndex)
