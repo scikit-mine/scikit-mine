14ba090ffc40020309c4c2a9515004e1573c7945
thomasbtnfr <thomas.betton@irisa.fr>
2022-11-04 16:11:03 +0100

    v0.0.7 (lcm)

--- skmine/itemsets/lcm.py
+++ skmine/itemsets/lcm.py
@@ -41,12 +41,10 @@ class LCM(BaseMiner, DiscovererMixin):
         Either an int representing the absolute support, or a float for relative support
         Default to 0.2 (20%)
 
-    n_jobs : int, default=1
-        The number of jobs to use for the computation. Each single item is attributed a job
-        to discover potential itemsets, considering this item as a root in the search space.
-        **Processes are preferred** over threads.
-        **Carefully adjust the number of jobs** otherwise the results may be corrupted especially if you have the following
-        warning: UserWarning: A worker stopped while some jobs were given to the executor.
+    n_jobs : int, default=1 The number of jobs to use for the computation. Each single item is attributed a job to
+    discover potential itemsets, considering this item as a root in the search space. **Processes are preferred**
+    over threads. **Carefully adjust the number of jobs** otherwise the results may be corrupted especially if you
+    have the following warning: UserWarning: A worker stopped while some jobs were given to the executor.
 
     max_depth: int, default=-1
         Maximum depth for exploration in the search space.
