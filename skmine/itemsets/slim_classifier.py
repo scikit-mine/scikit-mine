ce7ff3759ad7d9c62887c0de2aee787452331d6b
thomasbtnfr <thomas.betton@irisa.fr>
2022-12-27 11:52:41 +0100

    Add slim_classifier

---
+++ skmine/itemsets/slim_classifier.py
@@ -0,0 +1,85 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from .slim import SLIM
+import numpy as np
+from functools import reduce
+
+
+class SlimClassifier(BaseEstimator, ClassifierMixin):
+    """
+    Classifier using the SLIM compression algorithm. Works for binary and multi-class problems.
+
+    This classifier uses one SLIM instance per class in the database, resulting in a code table per class.
+    To classify a transaction, we simply assign the class belonging to the code table that provides the minimal encoded
+    length for the transaction.
+
+    Parameters
+    ----------
+    items: set, default=None
+        The list of items in the complete dataset not only the training set. This improves the accuracy of the model.
+        Without this set of items, the classifier works but is less good.
+
+    Attributes
+    ----------
+    classes_ : A list of all the classes
+    models_ : A list of SLIM instances corresponding to "classes_"
+    classes_X_ : A list where each element is a subset of X and each element contains the transactions of X associated
+                with the class from "classes_"  of the same index
+    """
+
+    def __init__(self, items=None):
+        self.models_ = None
+        self.classes_X_ = None
+        self.classes_ = None
+        self.items = items
+
+    def fit(self, X, y):
+        """Fit the model according to the given training data.
+        Parameters
+        ----------
+        X : containing n_transactions containing themselves n_items
+
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self : object
+            An instance of the estimator
+        """
+        self.classes_ = np.unique(y)
+        self.classes_X_ = []
+        self.models_ = []
+        for c in self.classes_:
+            transactions_classes = [transaction for transaction, target in zip(X, y) if target == c]
+            self.classes_X_.append(transactions_classes)
+            self.models_.append(SLIM(items=self.items))
+
+        for model, data in zip(self.models_, self.classes_X_):
+            model.fit(data)
+
+        return self
+
+    def predict(self, X):
+        """Perform classification on samples in X
+
+        Parameters
+        ----------
+        X : list of list containing n_transactions containing themselves n_items
+
+        Returns
+        -------
+        y_pred : np.array of shape (n_samples,)
+            Class labels for samples in X
+        """
+        if self.classes_ is None:
+            raise ValueError("fit must be called first.")
+
+        models_scores = {i: model.decision_function(X).values for i, model in enumerate(self.models_)}
+        predictions = []
+
+        for i in range(len(X)):
+            scores = [model[i] for model in models_scores.values()]
+            best_index = scores.index(reduce(lambda x, y: x if abs(y) > abs(x) else y, scores))
+            predictions.append(self.classes_[0] if best_index is None else self.classes_[best_index])
+
+        return np.array(predictions)
