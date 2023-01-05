7a1ec12ac9696988b329d3e4a923d474548f6c8e
hermann74 <hermann.courteille@irisa.fr>
2023-01-05 10:13:16 +0100

    remove instacart fetching (link for direct download is broken)

--- skmine/datasets/__init__.py
+++ skmine/datasets/__init__.py
@@ -3,6 +3,5 @@ The :mod:`skmine.datasets` module includes utilities to load datasets,
 including methods to load and fetch popular reference datasets.
 """
 from ._base import get_data_home
-from ._instacart import fetch_instacart
 from ._samples_generator import make_classification, make_transactions
 from .periodic import fetch_canadian_tv, fetch_health_app
