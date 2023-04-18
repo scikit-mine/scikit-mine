79f5581e95852b68f00853af2924cf1459019828
hermann74 <hermann.courteille@irisa.fr>
2023-04-18 17:04:00 +0200

    test for github action sur master

--- skmine/periodic/cycles.py
+++ skmine/periodic/cycles.py
@@ -144,6 +144,7 @@ class PeriodicPatternMiner(TransformerMixin, BaseEstimator):
             resolution = "nano-second"
             self.div_nb_sec_ = 1
         # TODO : merge self.div_nb_sec_ and self.n_zeros_ to just one number, by which nano-seconds are divided
+
         self.resolution = resolution
 
         if verbose:
